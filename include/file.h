5eee43894e5f473a79fd42b62799b5a0617993da
Aiden-Koorsen <aidenkoorsen@outlook.com>
2024-01-02 16:39:37 +0200

    Added documentation for strings and files

--- include/file.h
+++ include/file.h
@@ -1,3 +1,14 @@
+/**
+ * @file file.h
+ * @author Aiden Koorsen
+ * @brief Reading and writing with custom strings for default files
+ * @version 0.1
+ * @date 2024-01-02
+ * 
+ * @copyright MIT License
+ * 
+ */
+
 #ifndef FILE_H
 #define FILE_H
 
@@ -5,6 +16,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * @brief The new and improved structure file reading and writing 
+ * @param fp Pointer to original file
+ * @param name The name of the file
+ * @param success This boolean will allow you to test if the file has successfully read
+ */
 typedef struct 
 {
   FILE *fp;
@@ -12,19 +29,85 @@ typedef struct
   bool success;
 } helper_file;
 
+/**
+ * @brief Get the filename object
+ * 
+ * @param path Path to file you want to extract the file from
+ * @return string
+ */
 string get_filename(string path);
 
+/**
+ * @brief Opens a file and allows you to read it
+ * 
+ * @param path Path to file 
+ * @return helper_file 
+ */
 helper_file h_open_file(const char *path);
+
+/**
+ * @brief Opens a file and allow you to write to it 
+ * 
+ * @param path Path to file 
+ * @return helper_file 
+ */
 helper_file h_write_file(const char *path);
+
+/**
+ * @brief Closes and cleans up memory of the file 
+ * 
+ * @param f A pointer to the helper_file 
+ */
 void h_close_file(helper_file *f);
 
+/**
+ * @brief Reads a certain number of characters from a file 
+ * 
+ * @param f A pointer to the file 
+ * @param number The number of characters you want to read
+ * @return string 
+ */
 string read_characters(helper_file *f, int number);
+
+/**
+ * @brief Reads 1 line from a file 
+ * 
+ * @param f A pointer to the file 
+ * @return string 
+ */
 string read_line(helper_file *f);
 
+/**
+ * @brief Indicates if the file has been read completely or not 
+ * 
+ * @param f A pointer to the file 
+ * @return true/
+ * @return false 
+ */
 bool is_eof(helper_file f);
 
+/**
+ * @brief Writes a string to the file
+ * 
+ * @param f A pointer to the file 
+ * @param str The data you would like to write to
+ */
 void write_string(helper_file *f, string str);
+
+/**
+ * @brief Write a stirng to the file with a \n 
+ * 
+ * @param f A pointer to the file 
+ * @param ln The data you would like to write without a \n
+ */
 void write_line(helper_file *f, string ln);
+
+/**
+ * @brief Writes a c string to the file, this is sometimes better because with string, you need to free the memory after 
+ * 
+ * @param f A pointer to the file 
+ * @param data The data you would like to write to
+ */
 void write_chars(helper_file *f, char *data);
 
 #endif
--- include/str.h
+++ include/str.h
@@ -1,3 +1,13 @@
+/**
+ * @file str.h
+ * @author Aiden Koorsen
+ * @brief Contains a custom string type
+ * @version 0.1
+ * @date 2024-01-02
+ * 
+ * @copyright MIT License
+ * 
+ */
 #ifndef STRING_H
 #define STRING_H
 
@@ -5,23 +15,93 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * @brief A custom string data type 
+ * @param characters A pointer to an actual C-string
+ * @param length The length of the string
+ */
 typedef struct 
 {
   char *characters;
   int length;
 } string;
 
+/**
+ * @brief Creates a new string
+ * 
+ * @param c_str The data you would like that string to hold 
+ * @return string 
+ */
 string create_string(char *c_str);
+
+/**
+ * @brief Frees all the memory that the string holds
+ * 
+ * @param str A pointer to the string you would like to free 
+ */
 void free_string(string *str);
 
+/**
+ * @brief Finds the position of a string inside another string  
+ * 
+ * @param result The string you are searching for
+ * @param src The string you would like to search
+ * @return int - The index where the string was found
+ */
 int find_string(string result, string src);
+
+/**
+ * @brief Finds the position of a single character in a string 
+ * 
+ * @param result The character you are searching for
+ * @param src The string you would like to search
+ * @return int - The index where the character was found 
+ */
 int find_char(char result, string src);
+
+/**
+ * @brief Finds the position of a string that occurs last in another string 
+ * 
+ * @param result The string you are searching for
+ * @param src The string you would like to search
+ * @return int - The index where the last string was found
+ */
 int find_last_string(string result, string src);
+
+/**
+ * @brief Finds the position of a character that occurs last in a string
+ * 
+ * @param result The character you are searching for
+ * @param src The string you would like to search
+ * @return int - The index where the last chracter was found
+ */
 int find_last_char(char result, string src);
 
+/**
+ * @brief A wrapper for the strcmp function 
+ * 
+ * @param str1  
+ * @param str2 
+ * @return int - 0 = Same, 1 = str1 is greater, 2 = str1 is smaller
+ */
 int compare_strings(string str1, string str2);
 
+/**
+ * @brief Creates a copy of another string 
+ * 
+ * @param src The string you would like to copy
+ * @return string 
+ */
 string copy(string src);
+
+/**
+ * @brief Copies a certain amount of characters from a string starting at a certain position 
+ * 
+ * @param src The string you would like to copy
+ * @param index The offset within the string to copy from
+ * @param count The amount of characters you would to copy
+ * @return string 
+ */
 string copy_n(string src, int index, int count);
 
 #endif
